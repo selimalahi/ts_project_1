7ce0110bb7e885698ec99c97b7f38b7713b66097
selimalahi <alahi.selim@gmail.com>
2023-12-10 02:24:12 +0600

    filtering, sort, limit

--- src/modules/student/student.service.ts
+++ src/modules/student/student.service.ts
@@ -6,37 +6,66 @@ import { User } from '../user/user.model';
 import { TStudent } from './student.interface';
 
 const getAllStudentsFromDb = async (query: Record<string, unknown>) => {
+  console.log('base Query ', query);
+  const queryObj = { ...query }; //copy
 
- let searchTerm = '';
- if(query?.searchTerm){
-  searchTerm = query?.searchTerm as string;
- }
-
-  const result = await Student.find({
-  $or:['email', 'name.firstName', 'presentAddress'].map((field) =>({
-    [field]: {$regex: searchTerm, $options: 'i'}
-  }))
-  }).populate('admissionSemester').populate({
-    path: 'academicDepartment',
-    populate: 'academicFaculty',
+  const studentSearchableField = ['email', 'name.firstName', 'presentAddress'];
+  let searchTerm = '';
+  if (query?.searchTerm) {
+    searchTerm = query?.searchTerm as string;
+  }
+  const serachQuery = Student.find({
+    $or: studentSearchableField.map((field) => ({
+      [field]: { $regex: searchTerm, $options: 'i' },
+    })),
   });
-  return result;
-};
 
-const getSingleStudentFromDB = async (id: string) => {
-  // const result = await Student.aggregate([{ $match: { id: id } }]);
+  //filtering
+  const excluedFields = ['searchTerm', 'sort', 'limit'];
 
-  const result = await Student.findOne({ id })
+  excluedFields.forEach((el) => delete queryObj[el]);
+
+  const filterQuery = serachQuery
+    .find(queryObj)
     .populate('admissionSemester')
     .populate({
       path: 'academicDepartment',
       populate: 'academicFaculty',
     });
+    
+    let sort: string = '-createdAt';
+    if(query.sort){
+      sort = query.sort as string;
+    }
+
+    const sortQuery = filterQuery.sort(sort);
+
+    let limit = 1;
+    if(query.limit){
+      limit = query.limit ;
+    }
+
+    const limitQuery = await sortQuery.limit(limit)
+
+
+  return limitQuery;
+};
+
+const getSingleStudentFromDB = async (id: string) => {
+  // const result = await Student.aggregate([{ $match: { id: id } }]);
+
+  const result = Student.findOne({ id })
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: 'academicFaculty',
+    });  
+
   return result;
 };
 
 const updatedStudentIntoDB = async (id: string, payLoad: Partial<TStudent>) => {
-  const { name,guardian,  localGuardian, ...remainingStudentData } = payLoad;
+  const { name, guardian, localGuardian, ...remainingStudentData } = payLoad;
   const modifiedUpdateData: Record<string, unknown> = {
     ...remainingStudentData,
   };
@@ -59,14 +88,10 @@ const updatedStudentIntoDB = async (id: string, payLoad: Partial<TStudent>) => {
 
   console.log(modifiedUpdateData);
 
-  const result = await Student.findOneAndUpdate({ id },
-   modifiedUpdateData,
-   {
+  const result = await Student.findOneAndUpdate({ id }, modifiedUpdateData, {
     new: true,
-   runValidators: true
-  }
-   
-   );
+    runValidators: true,
+  });
   return result;
 };
 
